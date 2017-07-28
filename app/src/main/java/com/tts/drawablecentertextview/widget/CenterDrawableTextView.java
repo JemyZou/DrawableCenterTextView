5fb332c755d5ec7e82a72fccb6fd56bb12ee132a
jemy <13570302436@139.com>
2017-07-28 17:45:45 +0800

    fix bug：cannot use drawableleft and drawableright in the same xml

--- app/src/main/java/com/tts/drawablecentertextview/widget/CenterDrawableTextView.java
+++ app/src/main/java/com/tts/drawablecentertextview/widget/CenterDrawableTextView.java
@@ -2,7 +2,6 @@ package com.tts.drawablecentertextview.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
@@ -10,73 +9,70 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.TextView;
 
-import com.tts.drawablecentertextview.R;
-
 /**
  * Author: Jemy
- * Email: jemy.zou@easternphoenix.com
- * Time: 2017/7/28 14:51
  **/
 public class CenterDrawableTextView extends TextView {
-    public static final String LEFT="0";
-    public static final String RIGHT="1";
-    public static String LOCATION=LEFT;
+    private static final int LEFT = 0;//和getCompoundDrawables()数组角标一致
+    private static final int RIGHT = 2;
 
     public CenterDrawableTextView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public CenterDrawableTextView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public CenterDrawableTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context,attrs);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public CenterDrawableTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context, attrs);
-    }
-
-    private void init(Context context, AttributeSet attrs) {
-        //get drawable location
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CenterDrawableTextView);
-        String location = typedArray.getString(R.styleable.CenterDrawableTextView_pictureLocation);
-        if(LEFT.equalsIgnoreCase(location)){
-            LOCATION = LEFT;
-        } else if (RIGHT.equalsIgnoreCase(location)) {
-            LOCATION = RIGHT;
-        }
-        typedArray.recycle();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         float textWidth = getTextWidth();
         int compoundDrawablePadding = getCompoundDrawablePadding();
-        if (LOCATION.equalsIgnoreCase(LEFT)) {
-            drawableLeft(canvas,textWidth+compoundDrawablePadding);
-        } else if (LOCATION.equalsIgnoreCase(RIGHT)) {
-            drawableRight(canvas,textWidth+compoundDrawablePadding);
+        switch (getDrawablePosition()) {
+            case LEFT:
+                drawableLeft(canvas, textWidth + compoundDrawablePadding);
+                break;
+            case RIGHT:
+                drawableRight(canvas, textWidth + compoundDrawablePadding);
+                break;
         }
         super.onDraw(canvas);
     }
 
+    private int getDrawablePosition() {
+        Drawable[] drawables = getCompoundDrawables();
+        if (drawables == null) {
+            return LEFT;
+        }
+        if (drawables[LEFT] != null) {
+            return LEFT;
+        } else if (drawables[RIGHT] != null) {
+            return RIGHT;
+        }
+        return LEFT;
+    }
+
     private void drawableRight(Canvas canvas, float width) {
-        setGravity(Gravity.CENTER_VERTICAL|Gravity.RIGHT);
-        int drawableWidth=getDrawableWidth(2);
-        float transX= -(getWidth()-(width+drawableWidth))/2;
-        canvas.translate(transX,0);
+        setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
+        int drawableWidth = getDrawableWidth(2);
+        float transX = -(getWidth() - (width + drawableWidth)) / 2;
+        canvas.translate(transX, 0);
     }
 
     private void drawableLeft(Canvas canvas, float width) {
         setGravity(Gravity.CENTER_VERTICAL);
-        int drawableWidth=getDrawableWidth(0);
-        float transX = (getWidth()-(width+drawableWidth))/2;
-        canvas.translate(transX,0);
+        int drawableWidth = getDrawableWidth(0);
+        float transX = (getWidth() - (width + drawableWidth)) / 2;
+        canvas.translate(transX, 0);
     }
 
     private float getTextWidth() {
@@ -85,12 +81,11 @@ public class CenterDrawableTextView extends TextView {
 
     /**
      * @param position
-     * @return
-     * 获取drawable的宽度，position：left 0 top 1 right 2 bottom 3
+     * @return 获取drawable的宽度，position：left 0 top 1 right 2 bottom 3
      */
     private int getDrawableWidth(int position) {
         Drawable[] drawables = getCompoundDrawables();
-        if (drawables != null && drawables[position]!=null) {
+        if (drawables != null && drawables[position] != null) {
             return drawables[position].getIntrinsicWidth();
         }
         return 0;
